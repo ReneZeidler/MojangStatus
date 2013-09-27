c3e436a976a0466671af1d853411caeefd31fb00
ReneZeidler <rene.zeidler@gmx.de>
2013-09-27 18:38:16 +0200

    fix indentation

--- src/de/rene_zeidler/mojangstatus/CommandMCStatus.java
+++ src/de/rene_zeidler/mojangstatus/CommandMCStatus.java
@@ -7,42 +7,42 @@ import net.md_5.bungee.api.plugin.Command;
 public class CommandMCStatus extends Command {
 	MainConfig config;
 	
-    public CommandMCStatus()
-    {
-        super("mojangstatus", "mojangstatus.check", "mcstatus", "mcs");
-        config = MojangStatus.getInstance().getConfig();
-    }
+	public CommandMCStatus()
+	{
+		super("mojangstatus", "mojangstatus.check", "mcstatus", "mcs");
+		config = MojangStatus.getInstance().getConfig();
+	}
 
-    @Override
-    public void execute(CommandSender sender, String[] args)
-    {
-    	
-    	sender.sendMessage(parseMessage(config.commandCheckHeader));
-        sender.sendMessage(parseMessage(config.commandCheckStatusMinecraftNet, MojangStatus.minecraftNet, false));
-        sender.sendMessage(parseMessage(config.commandCheckStatusAuthserverMojang, MojangStatus.authserverMojang, false));
-        sender.sendMessage(parseMessage(config.commandCheckStatusSessionMinecraft, MojangStatus.sessionMinecraft, false));
-        sender.sendMessage(parseMessage(config.commandCheckStatusSkinsMinecraft, MojangStatus.skinsMinecraft, false));
-        if(!MojangStatus.sessionMinecraft) sender.sendMessage(parseMessage(config.commandCheckWarningSessionOffline));
-        else if(!MojangStatus.authserverMojang) sender.sendMessage(parseMessage(config.commandCheckWarningLoginOffline));
-        if(!MojangStatus.skinsMinecraft) sender.sendMessage(parseMessage(config.commandCheckWarningSkinsOffline));
-        sender.sendMessage(parseMessage(config.commandCheckHeader2));
-        sender.sendMessage(parseMessage(config.commandCheckStatusAuthMojang, MojangStatus.authMojang, true));
-        sender.sendMessage(parseMessage(config.commandCheckStatusAccountMojang, MojangStatus.accountMojang, true));
-        sender.sendMessage(parseMessage(config.commandCheckStatusLoginMinecraft, MojangStatus.loginMinecraft, true));
+	@Override
+	public void execute(CommandSender sender, String[] args)
+	{
+		
+		sender.sendMessage(parseMessage(config.commandCheckHeader));
+		sender.sendMessage(parseMessage(config.commandCheckStatusMinecraftNet, MojangStatus.minecraftNet, false));
+		sender.sendMessage(parseMessage(config.commandCheckStatusAuthserverMojang, MojangStatus.authserverMojang, false));
+		sender.sendMessage(parseMessage(config.commandCheckStatusSessionMinecraft, MojangStatus.sessionMinecraft, false));
+		sender.sendMessage(parseMessage(config.commandCheckStatusSkinsMinecraft, MojangStatus.skinsMinecraft, false));
+		if(!MojangStatus.sessionMinecraft) sender.sendMessage(parseMessage(config.commandCheckWarningSessionOffline));
+		else if(!MojangStatus.authserverMojang) sender.sendMessage(parseMessage(config.commandCheckWarningLoginOffline));
+		if(!MojangStatus.skinsMinecraft) sender.sendMessage(parseMessage(config.commandCheckWarningSkinsOffline));
+		sender.sendMessage(parseMessage(config.commandCheckHeader2));
+		sender.sendMessage(parseMessage(config.commandCheckStatusAuthMojang, MojangStatus.authMojang, true));
+		sender.sendMessage(parseMessage(config.commandCheckStatusAccountMojang, MojangStatus.accountMojang, true));
+		sender.sendMessage(parseMessage(config.commandCheckStatusLoginMinecraft, MojangStatus.loginMinecraft, true));
 }
-    
-    private String parseMessage(String m)
-    {
-    	return ChatColor.translateAlternateColorCodes('&', m);
-    			
-    }
-    
-    private String parseMessage(String m, boolean online, boolean light)
-    {
-    	return ChatColor.translateAlternateColorCodes('&', m.replace("%status%",
-    			(online ? ((light ? ChatColor.GREEN : ChatColor.DARK_GREEN) + "online")
-    					: ((light ? ChatColor.RED : ChatColor.DARK_RED) + "offline"))));
-    			
-    }
-    
+	
+	private String parseMessage(String m)
+	{
+		return ChatColor.translateAlternateColorCodes('&', m);
+				
+	}
+	
+	private String parseMessage(String m, boolean online, boolean light)
+	{
+		return ChatColor.translateAlternateColorCodes('&', m.replace("%status%",
+				(online ? ((light ? ChatColor.GREEN : ChatColor.DARK_GREEN) + "online")
+						: ((light ? ChatColor.RED : ChatColor.DARK_RED) + "offline"))));
+				
+	}
+	
 }
--- src/de/rene_zeidler/mojangstatus/CommandSetStatus.java
+++ src/de/rene_zeidler/mojangstatus/CommandSetStatus.java
@@ -6,37 +6,37 @@ import net.md_5.bungee.api.plugin.Command;
 
 public class CommandSetStatus extends Command {
 	
-    public CommandSetStatus()
-    {
-        super("setstatus", "mojangstatus.set", "setmcs");
-    }
+	public CommandSetStatus()
+	{
+		super("setstatus", "mojangstatus.set", "setmcs");
+	}
 
-    @Override
-    public void execute(CommandSender sender, String[] args)
-    {
-    	if(args.length != 2) {
-    		sender.sendMessage(ChatColor.RED  + "Wrong number of arguments. Use /setstatus <server> <online|offline>");
-    		return;
-    	}
-    	boolean online = false;
-    	if(args[1].equalsIgnoreCase("online") || args[1].equalsIgnoreCase("true")) online = true;
-    	else if(args[1].equalsIgnoreCase("offline") || args[1].equalsIgnoreCase("false")) online = false;
-    	else {
-    		sender.sendMessage(ChatColor.RED  + "Second argument must be \"online\" or \"offline\"");
-    		return;
-    	}
-    	if(args[0].equalsIgnoreCase("minecraftNet") || args[0].equalsIgnoreCase("website") || args[0].equalsIgnoreCase("minecraft.net")) MojangStatus.getInstance().setMinecraftNetStatus(online);
-    	else if(args[0].equalsIgnoreCase("authServerMojang") || args[0].equalsIgnoreCase("login") || args[0].equalsIgnoreCase("authserver.mojang.com")) MojangStatus.getInstance().setAuthServerMojangStatus(online);
-    	else if(args[0].equalsIgnoreCase("sessionMinecraft") || args[0].equalsIgnoreCase("session") || args[0].equalsIgnoreCase("session.minecraft.net")) MojangStatus.getInstance().setSessionMinecraftStatus(online);
-    	else if(args[0].equalsIgnoreCase("skinsMinecraft") || args[0].equalsIgnoreCase("skins") || args[0].equalsIgnoreCase("skins.minecraft.net")) MojangStatus.getInstance().setSkinsMinecraftStatus(online);
-    	else if(args[0].equalsIgnoreCase("authMojang") || args[0].equalsIgnoreCase("auth") || args[0].equalsIgnoreCase("auth.mojang.com")) MojangStatus.getInstance().setAuthMojangStatus(online);
-    	else if(args[0].equalsIgnoreCase("accountMojang") || args[0].equalsIgnoreCase("account") || args[0].equalsIgnoreCase("account.mojang.com")) MojangStatus.getInstance().setAccountMojangStatus(online);
-    	else if(args[0].equalsIgnoreCase("loginMinecraft") || args[0].equalsIgnoreCase("legacylogin") || args[0].equalsIgnoreCase("login.minecraft.net")) MojangStatus.getInstance().setLoginMinecraftStatus(online);
-    	else {
-    		sender.sendMessage(ChatColor.RED  + "Unknown server, please use the server address or the service name");
-    		return;
-    	}
+	@Override
+	public void execute(CommandSender sender, String[] args)
+	{
+		if(args.length != 2) {
+			sender.sendMessage(ChatColor.RED  + "Wrong number of arguments. Use /setstatus <server> <online|offline>");
+			return;
+		}
+		boolean online = false;
+		if(args[1].equalsIgnoreCase("online") || args[1].equalsIgnoreCase("true")) online = true;
+		else if(args[1].equalsIgnoreCase("offline") || args[1].equalsIgnoreCase("false")) online = false;
+		else {
+			sender.sendMessage(ChatColor.RED  + "Second argument must be \"online\" or \"offline\"");
+			return;
+		}
+		if(args[0].equalsIgnoreCase("minecraftNet") || args[0].equalsIgnoreCase("website") || args[0].equalsIgnoreCase("minecraft.net")) MojangStatus.getInstance().setMinecraftNetStatus(online);
+		else if(args[0].equalsIgnoreCase("authServerMojang") || args[0].equalsIgnoreCase("login") || args[0].equalsIgnoreCase("authserver.mojang.com")) MojangStatus.getInstance().setAuthServerMojangStatus(online);
+		else if(args[0].equalsIgnoreCase("sessionMinecraft") || args[0].equalsIgnoreCase("session") || args[0].equalsIgnoreCase("session.minecraft.net")) MojangStatus.getInstance().setSessionMinecraftStatus(online);
+		else if(args[0].equalsIgnoreCase("skinsMinecraft") || args[0].equalsIgnoreCase("skins") || args[0].equalsIgnoreCase("skins.minecraft.net")) MojangStatus.getInstance().setSkinsMinecraftStatus(online);
+		else if(args[0].equalsIgnoreCase("authMojang") || args[0].equalsIgnoreCase("auth") || args[0].equalsIgnoreCase("auth.mojang.com")) MojangStatus.getInstance().setAuthMojangStatus(online);
+		else if(args[0].equalsIgnoreCase("accountMojang") || args[0].equalsIgnoreCase("account") || args[0].equalsIgnoreCase("account.mojang.com")) MojangStatus.getInstance().setAccountMojangStatus(online);
+		else if(args[0].equalsIgnoreCase("loginMinecraft") || args[0].equalsIgnoreCase("legacylogin") || args[0].equalsIgnoreCase("login.minecraft.net")) MojangStatus.getInstance().setLoginMinecraftStatus(online);
+		else {
+			sender.sendMessage(ChatColor.RED  + "Unknown server, please use the server address or the service name");
+			return;
+		}
 		sender.sendMessage(ChatColor.GREEN  + "Successfully set status of " + ChatColor.GOLD + args[0] + ChatColor.GREEN + " to " + (online ? (ChatColor.DARK_GREEN + "online") : (ChatColor.DARK_RED + "offline")));
-    }
-    
+	}
+	
 }
